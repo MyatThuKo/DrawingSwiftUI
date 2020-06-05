d5e989756a15cfedec8d9eefcce3969769626750
Myat Thu Ko <nymyatthuko@gmail.com>
2020-06-05 16:33:21 -0400

    Completed the challenge

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct Rectangle: Shape {
+struct MyRectangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
@@ -35,41 +35,86 @@ struct Triangle: Shape {
     }
 }
 
+struct ColorCyclingRectangle: View {
+    
+    var amount = 0.0
+    var steps = 400
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    .fill(self.color(for: value, brightness: 1))
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+    
+}
+
 struct ContentView: View {
     
     @State private var rotationDegree: Double = 0
-    @State private var lineWidth: CGFloat = 10
+    @State private var lineWidth: CGFloat = 5
+    @State private var showGradient = false
+    @State private var colorCycle = 0.0
     
     var body: some View {
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [.yellow, .orange]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            VStack {
-                Spacer()
-                ZStack {
-                    Triangle()
-                        .stroke(Color.red, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                        .frame(width: 90, height: 100)
-                    Rectangle()
-                        .stroke(Color.red, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                        .frame(width: 30, height: 200)
-                        .offset(y: 150)
+        NavigationView {
+            ZStack {
+                if showGradient {
+                    ColorCyclingRectangle(amount: self.colorCycle)
+                        .edgesIgnoringSafeArea(.all)
+                } else {
+                    LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        .edgesIgnoringSafeArea(.all)
                 }
-                .rotationEffect(.degrees(rotationDegree))
-                .animation(.spring())
-                .offset(x: 0, y: -300)
-                
-                Group {
-                    Text("Degree: \(self.rotationDegree, specifier: "%g")")
-                    Slider(value: $rotationDegree, in: 0...360, step: 1)
-                        .padding([.horizontal, .bottom])
+                VStack {
+                    Spacer()
+                    ZStack {
+                        Triangle()
+                            .stroke(Color.white, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+                            .frame(width: 45, height: 50)
+                        MyRectangle()
+                            .stroke(Color.white, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+                            .frame(width: 20, height: 100)
+                            .offset(y: 75)
+                    }
+                    .rotationEffect(.degrees(rotationDegree))
+                    .animation(.spring())
+                    .offset(x: 0, y: -250)
                     
-                    Text("Line Width: \(self.lineWidth, specifier: "%g")")
-                    Slider(value: $lineWidth, in: 0...25, step: 1)
-                        .padding([.horizontal, .bottom])
+                    Group {
+                        Text("Degree: \(self.rotationDegree, specifier: "%g")")
+                        Slider(value: $rotationDegree, in: -360...360, step: 1)
+                            .padding(.horizontal)
+                        
+                        Text("Line Width: \(self.lineWidth, specifier: "%g")")
+                        Slider(value: $lineWidth, in: 1...15, step: 1)
+                            .padding(.horizontal)
+                        
+                        Text("Gradient Controller:")
+                        Slider(value: $colorCycle)
+                            .padding(.horizontal)
+                        
+                    }
                 }
             }
+            .navigationBarItems(trailing: Toggle(isOn: $showGradient) {
+                Text("Show Gradient")
+            })
         }
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
